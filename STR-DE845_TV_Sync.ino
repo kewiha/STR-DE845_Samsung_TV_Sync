df3c89814bb70b5b5d102efddf5d9c81f9247e29
Keith Harrison <keithh@protonmail.com>
2024-07-07 10:25:47 -0400

    fix resyncing if TV turns on/off w/o IR code being used/read

--- STR-DE845_TV_Sync.ino
+++ STR-DE845_TV_Sync.ino
@@ -6,18 +6,22 @@
 #define NO_LED_FEEDBACK_CODE
 #include <IRremote.hpp>  // include the library
 
+unsigned int millis_var;
+
 // Power details
 #define TV_POWER_PIN 5
 #define STR_DE845_POWER_PIN 6
 bool TV_power_on[2]; //index 0 = past, index 1 = now
 bool STR_DE845_power_on[2]; //index 0 = past, index 1 = now
+#define wait_before_power_state_sync_ms 2000
 
 // TV Remote Details
 #define TV_Remote_Proto SAMSUNG
-const uint16_t TV_Remote_Address = 0x707;
+const uint16_t TV_Remote_Address = 0x007;
 
 // Sony STR-DE845 Details
 const uint16_t Control_S_Address = 16;
+unsigned int last_Control_S_sent_time_ms = 0;
 
 // Mapping of TV Remote commands to Control-S commands
 const uint8_t mapped_commands = 3;
@@ -51,12 +55,12 @@ void setup() {
   Serial.println("### Setting up TV and STR-DE845 power status pins ###");
   Serial.print(" TV_POWER_PIN=");
   Serial.println(TV_POWER_PIN);
-  pinMode(TV_POWER_PIN,INPUT);
+  pinMode(TV_POWER_PIN,INPUT); //External pulldown resistor needed. My TV cuts power to USB port shortly after power off, but the result is a floating input
   TV_power_on[0]=digitalRead(TV_POWER_PIN);
   TV_power_on[1]=TV_power_on[0];
   Serial.print(" STR_DE845_POWER_PIN=");
   Serial.println(STR_DE845_POWER_PIN);
-  pinMode(STR_DE845_power_on,INPUT);
+  pinMode(STR_DE845_power_on,INPUT); //External pulldown resistor needed. Lower resistance (without exceeding safe current limits for Arduino) speeds up 5V -> 0V transition
   STR_DE845_power_on[0]=digitalRead(TV_POWER_PIN);
   STR_DE845_power_on[1]=STR_DE845_power_on[0];
 
@@ -66,19 +70,23 @@ void setup() {
 void loop() {
   //IR Input check
   if (IrReceiver.decode()) {
-    Serial.print("Recv IR code. ");
+    Serial.print("Recv IR code. Proto: ");
+    Serial.print(IrReceiver.decodedIRData.protocol);
     if (IrReceiver.decodedIRData.protocol == TV_Remote_Proto) {
-      Serial.print("Proto ok. ");
+      Serial.print(" ok. Addr: ");
+      Serial.print(IrReceiver.decodedIRData.address);
       if(IrReceiver.decodedIRData.address == TV_Remote_Address){
-      Serial.print("Addr ok. ");
+      Serial.print(" ok. Cmd: ");
+      Serial.print(IrReceiver.decodedIRData.command);
       for(uint8_t iCmd = 0; iCmd<=mapped_commands - 1; iCmd++)
         if(IrReceiver.decodedIRData.command == command_mapping[iCmd][0]){
-          Serial.print("Cmd ok (");
+          Serial.print(" ok (");
           Serial.print(command_mapping[iCmd][0]);
           Serial.print("). Sending ");
           Serial.print(command_mapping[iCmd][1]);
           Serial.print(" as ");
           Control_S_send(Control_S_Address, command_mapping[iCmd][1],command_mapping[iCmd][2]);
+          last_Control_S_sent_time_ms = millis();
         }
       }
     }
@@ -86,16 +94,37 @@ void loop() {
     IrReceiver.resume();
   }
 
-  //TV or STR_DE845 state check
-  TV_power_on[1]=digitalRead(TV_POWER_PIN);
-  STR_DE845_power_on[1]=digitalRead(TV_POWER_PIN);
-  if(TV_power_on[1]!=TV_power_on[0] && TV_power_on[1]==0){ //TV turned off
-    if(STR_DE845_power_on[1]==1){ //STR-DE845 is still on
-      Control_S_send(Control_S_Address, command_mapping[Control_S_power_map_id][1],command_mapping[Control_S_power_map_id][2]);
-    }
-  } else if(TV_power_on[1]!=TV_power_on[0] && TV_power_on[1]==1){ //TV turned on
-    if(STR_DE845_power_on[1]==0){ //STR-DE845 is still off
-      Control_S_send(Control_S_Address, command_mapping[Control_S_power_map_id][1],command_mapping[Control_S_power_map_id][2]);
+  //TV or STR_DE845 sync check (e.g. in case TV turns off automatically, need to turn off STR-DE845 so devices are both on or both off)
+  millis_var = millis();
+  if (millis_var > last_Control_S_sent_time_ms) { //Ignore state checks when millis() just rolled over to 0
+    if ((millis_var - last_Control_S_sent_time_ms) > wait_before_power_state_sync_ms ) { //Don't act on TV_POWER_PIN or STR_DE845_POWER_PIN right away in case STR-DE845 is already acting on a Control S signal
+      TV_power_on[1]=digitalRead(TV_POWER_PIN);
+      STR_DE845_power_on[1]=digitalRead(STR_DE845_POWER_PIN);
+      if(TV_power_on[1]!=TV_power_on[0] && TV_power_on[1]==0){ //TV turned off
+        Serial.print("TV turned off.");
+        if(STR_DE845_power_on[1]==1){ //STR-DE845 is still on
+          Serial.println(" STR-DE845 is still on, turning off.");
+          Control_S_send(Control_S_Address, command_mapping[Control_S_power_map_id][1],command_mapping[Control_S_power_map_id][2]);
+          STR_DE845_power_on[1]=!STR_DE845_power_on[1];
+          last_Control_S_sent_time_ms = millis();
+        } else {
+          Serial.println(" STR-DE845 is off, nothing to do.");
+          last_Control_S_sent_time_ms = millis(); //Dirty hack: no Control S signal sent
+        }
+        TV_power_on[0]=TV_power_on[1];
+      } else if(TV_power_on[1]!=TV_power_on[0] && TV_power_on[1]==1){ //TV turned on
+        Serial.print("TV turned on");
+        if(STR_DE845_power_on[1]==0){ //STR-DE845 is still off
+          Serial.println(" STR-DE845 is still off, turning on.");
+          Control_S_send(Control_S_Address, command_mapping[Control_S_power_map_id][1],command_mapping[Control_S_power_map_id][2]);
+          STR_DE845_power_on[1]=!STR_DE845_power_on[1];
+          last_Control_S_sent_time_ms = millis();
+        } else {
+          Serial.println(" STR-DE845 is on, nothing to do.");
+          last_Control_S_sent_time_ms = millis(); //Dirty hack: no Control S signal sent
+        }
+        TV_power_on[0]=TV_power_on[1];
+      }
     }
   }
 };
